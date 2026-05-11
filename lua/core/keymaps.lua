a7052d8d777ec8ba9ce3e32b7411cdb20b870183
Glauber Brennon <glauberbrennon@gmail.com>
2026-05-11 17:45:29 -0300

    feat: add keymap to open a terminal session

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,6 +17,7 @@ function M.setup()
   map("n", "<leader>W", ":wa<CR>", { desc = "Save all files" })
   map("n", "<leader>Q", ":qa!<CR>", { desc = "Force quit all" })
   map("n", "<leader><Space>", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+  map("n", "<leader>t", ":terminal", { desc = "Starts terminal session" })
 
   -- Toggle relative line numbers
   map("n", "<leader>trn", function()
